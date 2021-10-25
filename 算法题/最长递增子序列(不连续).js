69d074cc03647dd9b4a88ec1363dc28ece00dc3a
unknown <cuiyiml@163.com>
2021-10-25 13:01:53 +0800

    update

--- 算法题/最长递增子序列(不连续).js
+++ 算法题/最长递增子序列(不连续).js
@@ -1,17 +1,25 @@
+// 数组[0, 3, 1, 6, 2, 2, 7],
+// 子序列有[3,6,2,7],不连续
 function LIS(arr) {
     let dp = new Array(arr.length).fill(1);
+    let longStr = new Array(arr.length).fill([])
     let res = 1
     for (let i = 0; i < arr.length; i++) {
+        longStr[i] = [arr[i]]
         for (let j = 0; j < i; j++) {
             if (arr[i] > arr[j]) {
+                if (dp[j] + 1 > dp[i]) {
+                    longStr[i] = [...longStr[j], arr[i]];
+                }
                 dp[i] = Math.max(dp[j] + 1, dp[i]);
             }
         }
         res = Math.max(dp[i], res)
     }
+    console.log(longStr);
     console.log(dp);
     return res
 }
-let arr = [0, 1, 0, 3, 2]
+let arr = [0, 3, 1, 6, 2, 2, 7] //[0,3,6,7]
 let res = LIS(arr);
 console.log(res);
